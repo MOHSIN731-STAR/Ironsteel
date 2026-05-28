8aaf1c96ab551dda5560f203f7d53f602692475f
77MMOHSIN <mohsinriazjmpr@gmail.com>
2026-05-28 14:05:46 +0500

    final code is done

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,8 +1,12 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-})
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = new PrismaClient({ adapter })
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
